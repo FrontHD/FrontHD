b134dcd02a04df75fe48eb0617d0cd17322c4263
FrontHD <sander.utgard.reppen@gmail.com>
2018-12-01 16:57:31 +0100

    Tweenservice (smooth animation)

--- Code.lua
+++ Code.lua
@@ -1,2 +1,7 @@
-print("hello")
-print("good bye")
+local Object = game.Part
+local TweenService = game:GetService("TweenService")
+local Tweeninfo = TweenInfo.new(Enum.EasingStyle.Sine,Enum.EasingDirection.InOut,0,false,0)
+local Goal1 = {Position = Vector3.new(1,0,0)}
+local Goal2 = {Position = Vector3.new(0,0,0)}
+local PlayGoal1 = TweenService:Create(Object,TweenInfo,Goal1)
+local PlayGoal2 = TweenService:Create(Object,TweenInfo,Goal2)
