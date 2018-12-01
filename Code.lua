f2643a3d0de1cb879202998c6c942ea17c171663
Bj°rn Reppen <bjornreppen@users.noreply.github.com>
2018-12-01 17:00:20 +0100

    Updated Tweenservice

--- Code.lua
+++ Code.lua
@@ -5,3 +5,10 @@ local Goal1 = {Position = Vector3.new(1,0,0)}
 local Goal2 = {Position = Vector3.new(0,0,0)}
 local PlayGoal1 = TweenService:Create(Object,TweenInfo,Goal1)
 local PlayGoal2 = TweenService:Create(Object,TweenInfo,Goal2)
+
+while true do
+    PlayGoal1:Play()
+    wait(1)
+    PlayGoal2:Play()
+    wait(1)
+end
